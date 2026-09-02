d89835c46ca32c9bdc238b91ea0a832404ee4b2b
Amine Ben Salha <medaminebslh@gmail.com>
2026-09-02 18:45:01 +0200

    feat: complete Utilisateur entity with Lombok

---
+++ src/main/java/com/cabinetkine/cabinet_kine_backend/model/Utilisateur.java
@@ -0,0 +1,22 @@
+package com.cabinetkine.cabinet_kine_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Utilisateur {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String nom;
+    private String role;
+}
