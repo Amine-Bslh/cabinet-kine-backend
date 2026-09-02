481ba1ca206f0268cbc53e86984c9d5c9a6cb44f
Amine Ben Salha <medaminebslh@gmail.com>
2026-09-02 17:54:03 +0200

    chore: setup projet Spring Boot

---
+++ src/main/java/com/cabinetkine/cabinet_kine_backend/CabinetKineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cabinetkine.cabinet_kine_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabinetKineBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabinetKineBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cabinetkine/cabinet_kine_backend/CabinetKineBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cabinetkine.cabinet_kine_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabinetKineBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
